549d5c417610ce4153467373dd69490f647924a3
Jo Greybill <jogreybill@gmail.com>
2013-04-22 09:15:03 -0400

    Bootstrap ref to base branch; removed some unneeded generic theme stuff; added address theme option fields

--- single.php
+++ single.php
@@ -1,8 +1,7 @@
 <?php disallow_direct_load('single.php');?>
 <?php get_header(); the_post();?>
-	
 	<div class="row page-content" id="<?=$post->post_name?>">
-		<div class="span9">
+		<div class="span12">
 			<article>
 				<? if(!is_front_page())	{ ?>
 						<h1><?php the_title();?></h1>
@@ -10,12 +9,10 @@
 				<?php the_content();?>
 			</article>
 		</div>
-		
-		<div id="sidebar" class="span3">
-			<?=get_sidebar();?>
-		</div>
 	</div>
-		
-	<?php get_template_part('includes/below-the-fold'); ?>
-
+	<?
+	if(get_post_meta($post->ID, 'page_hide_fold', True) != 'on'): 
+		get_template_part('includes/below-the-fold'); 
+	endif
+	?>
 <?php get_footer();?>
