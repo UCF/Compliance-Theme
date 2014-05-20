b187870b16edef8975aa9b570bb4ef2a9f043c36
Jo Dickson <jogreybill@gmail.com>
2014-05-20 17:01:10 -0400

    Re-enabled below the fold; added news to homepage

--- home.php
+++ home.php
@@ -3,17 +3,34 @@
 <?php
 	$options 		= get_option(THEME_OPTIONS_NAME);
 	$home_content 	= apply_filters('the_content', $options['home_content']);
+	$news_stories   = get_posts(array(
+						'numberposts' => 3,
+						'orderby'     => 'date',
+						'order'       => 'DESC',
+						'post_type'   => 'post'
+						)
+					);
 ?>
 	<div class="row page-content" id="home">
-		<div class="span12">
+		<div class="span9">
 			<article>
 				<?=$home_content;?>
 			</article>
 		</div>
+		<div class="span3" id="announcements">
+			<h3>News &amp; Updates</h3>
+			<ul class="unstyled">
+			<?php foreach($news_stories as $story) { ?>
+				<li>
+					<a href="<?=get_page_link($story->ID)?>"><?=$story->post_title?></a>
+				</li>
+			<?php } ?>
+			</ul>
+		</div>
+		<hr class="span12" />
 	</div>
 	<?=get_home_featured_content();?>
 	
-<!--
-<// ?=get_below_the_fold();?>
--->
+<?=get_below_the_fold();?>
+
 <?php get_footer();?>
