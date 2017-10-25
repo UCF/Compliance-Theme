392a36b210c4aff13dbece53ab351a27177a137c
samuraijaffy <jaffyescarcha@Jaffys-MacBook-Pro.local>
2017-10-25 16:06:03 -0400

    Implement New Layout / Design for Code of Conduct Section

--- functions.php
+++ functions.php
@@ -46,21 +46,21 @@ function get_below_the_fold($post_id=null) {
 		ob_start();
 		?>
 		<div class="row" id="below-the-fold">
-			<div class="span4" id="below-the-fold-icon">
-				<p><a href="<?=do_shortcode($link);?>"><img src="<?=THEME_IMG_URL?>/below-the-fold.png" alt="Speak up when you know of or suspect unethical behavior." title="<?=$title?>" /></a></p>
-			</div>
-			<div class="span4" id="below-the-fold-content">
-				<h3><?=$title?></h3>
-				<?=$blurb?>
-			</div>
-			<div class="span4" id="code-of-conduct">
+			<div class="span6" id="code-of-conduct">
 				<div class="row-fluid">
-					<div class="span5"><a href="<?=do_shortcode($ucfcclink);?>" target="_blank"><img src="<?=THEME_IMG_URL?>/UCF-CodeOfConduct.jpg" alt="<?=$ucfcccta?>" title="<?=$ucfcccta?>" /></a></div>
-					<div class="span7" id="code-of-conduct-content">
+					<div class="span4"><a href="<?=do_shortcode($ucfcclink);?>" target="_blank"><img src="<?=THEME_IMG_URL?>/UCF-CodeOfConduct.jpg" alt="<?=$ucfcccta?>" title="<?=$ucfcccta?>" /></a></div>
+					<div class="span8" id="code-of-conduct-content">
 						<h3><?=$ucfcccta?></h3>
 						<a href="<?=do_shortcode($ucfcclink);?>" class="btn" target="_blank"><?=$ucfccbtn?></a>
 					</div>
 				</div>
+			</div>
+			<div class="span3" id="below-the-fold-icon">
+				<p><a href="<?=do_shortcode($link);?>"><img src="<?=THEME_IMG_URL?>/below-the-fold.png" alt="Speak up when you know of or suspect unethical behavior." title="<?=$title?>" /></a></p>
+			</div>
+			<div class="span3" id="below-the-fold-content">
+				<h3><?=$title?></h3>
+				<?=$blurb?>
 			</div>
 		</div>
 		<?php
